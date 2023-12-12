68c62345a062e58f90a6501ade6fa1d91bb3f96f
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2023-12-12 00:22:00 -0800

    Add files via upload

---
+++ controllers/users.js
@@ -0,0 +1,56 @@
+const Users = require("../models/user");
+const { NOT_FOUND, SERVER_ERROR, INVALID_DATA } = require("../utils/errors");
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await Users.find({});
+    res.send({ data: users });
+  } catch (err) {
+    // console.error(`Error getUsers ${err.name} with the message ${err.message} has occurred while executing the code`);
+    res
+      .status(SERVER_ERROR)
+      .send({ message: "An error has occurred on the server." });
+  }
+};
+
+const getUser = async (req, res) => {
+  try {
+    const user = await Users.findById(req.params.userId);
+    if (user) {
+      res.send({ data: user });
+    } else {
+      res.status(NOT_FOUND).send({ message: "User not found" });
+    }
+  } catch (err) {
+    // console.error(`Error getUser ${err.name} with the message ${err.message} has occurred while executing the code`);
+    if (err.name === "CastError") {
+      res.status(INVALID_DATA).send({ message: "Invalid user ID provided" });
+    } else {
+      res
+        .status(SERVER_ERROR)
+        .send({ message: "An error has occurred on the server." });
+    }
+  }
+};
+
+const createUser = async (req, res) => {
+  const { name, avatar } = req.body;
+  try {
+    const user = await Users.create({ name, avatar });
+    res.send({ data: user });
+  } catch (err) {
+    // console.error(`Error createUser ${err.name} with the message ${err.message} has occurred while executing the code`);
+    if (err.name === "ValidationError") {
+      res
+        .status(INVALID_DATA)
+        .send({ message: "Invalid data provided for creating a user" });
+    } else {
+      res
+        .status(SERVER_ERROR)
+        .send({ message: "An error has occurred on the server." });
+    }
+  }
+};
+
+module.exports = { getUsers, getUser, createUser };
+
---
+++ utils/errors.js
@@ -0,0 +1,7 @@
+module.exports = {
+  INVALID_DATA: 400,
+  NOT_FOUND: 404,
+  SERVER_ERROR: 500,
+  CREATED: 201,
+  OK: 200,
+};
