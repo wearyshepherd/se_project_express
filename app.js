9ad644d709d4949a91498a7137c3e6d8ad5221b9
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2023-12-18 19:41:43 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
-const {login, createUser} = require('./controllers/users');
+const { login, createUser } = require('./controllers/users');
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -21,8 +21,6 @@ app.post('/signin', login);
 
 app.use(routes);
 
-
-
 app.listen(PORT, () => {
-  console.log(`App listening at port ${PORT}`);
+  // Removed console.log statement
 });
