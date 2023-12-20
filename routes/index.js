9b64bf95ca1dc3e3f9067a84116a8ba9a9582d72
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2023-12-19 22:24:26 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
 const router = require("express").Router();
-// const clothingItem = require("./clothingItem");
-// const user = require("./users");
-const authMiddleware = require("../middlewares/auth");
+const clothingItem = require("./clothingItem");
+const user = require("./users");
 const { NOT_FOUND } = require("../utils/errors");
-
-router.use(authMiddleware);
 
 router.use((req, res) => {
   res.status(NOT_FOUND).send({ message: "Route not found" });
