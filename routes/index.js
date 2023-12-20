0143e4ac91d86cc52ba9ff76f802bf679ae00c72
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2023-12-20 00:54:31 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const clothingItem = require("./clothingItem");
 const user = require("./users");
 const { NOT_FOUND } = require("../utils/errors");
 
+router.use("/items", clothingItem);
+router.use("/users", user);
+
 router.use((req, res) => {
   res.status(NOT_FOUND).send({ message: "Route not found" });
 });
