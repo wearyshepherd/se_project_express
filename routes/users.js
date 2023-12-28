41803dac167e00f3a293f89195633cf64629debe
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2023-12-27 22:39:51 -0800

    Delete routes directory

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const authMiddleware = require('../middlewares/auth');
-
-const {getCurrentUser, updateUserProfile} = require('../controllers/users');
-
-router.get('/me', authMiddleware, getCurrentUser);
-router.patch('/me', authMiddleware, updateUserProfile);
-
-module.exports = router;
