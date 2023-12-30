18027e048f7d509fb84d49b6420f46cb9478137b
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2023-12-29 21:24:31 -0800

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,15 @@
-const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = require('../utils/config');
-const { UNAUTHORIZED } = require('../utils/errors');
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../utils/config");
+const { UnauthorizedError } = require("../utils/errors/UnauthorizedError");
 
 const authMiddleware = (req, res, next) => {
   const authorizationHeader = req.headers.authorization;
 
-  if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
-    return res.status(UNAUTHORIZED).send({ message: 'Unauthorized' });
+  if (!authorizationHeader || !authorizationHeader.startsWith("Bearer ")) {
+    throw new UnauthorizedError("Unauthorized");
   }
 
-  const token = authorizationHeader.replace('Bearer ', '');
+  const token = authorizationHeader.replace("Bearer ", "");
 
   try {
     const payload = jwt.verify(token, JWT_SECRET);
@@ -18,7 +18,7 @@ const authMiddleware = (req, res, next) => {
 
     return next();
   } catch (e) {
-    return res.status(UNAUTHORIZED).send({ message: 'Unauthorized' });
+    throw new UnauthorizedError("Unauthorized");
   }
 };
 
