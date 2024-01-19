3b74563b3d941802171c58e0914559c4be97f30d
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2024-01-18 18:17:44 -0800

    Add files via upload

---
+++ utils/errors/BadRequestError.js
@@ -0,0 +1,11 @@
+class BadRequestError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "BadRequestError";
+    this.status = 400;
+  }
+}
+
+module.exports = {
+  BadRequestError,
+};
---
+++ utils/errors/NotFoundError.js
@@ -0,0 +1,11 @@
+class NotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "NotFoundError";
+    this.status = 404;
+  }
+}
+
+module.exports = {
+  NotFoundError,
+};
